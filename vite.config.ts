49d21bb12a6bcfe74131187f22d599aa31d0e5e3
Damir Shamanaev <damirka.ru@gmail.com>
2026-02-26 15:04:37 -0600

    Revert "try patching vite"
    This reverts commit ea73fa7e0a90c604b0fa7e45ee98f31ad8c99d96.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/display-preview/',
   plugins: [react(), tailwindcss()],
 })
